164526eec6cd89913225c9081e1ef3d782f37105
melodyjing <meljel142857@gmail.com>
2023-11-30 11:44:46 -0600

    Added 2 game files.

---
+++ RecursionGames.py
@@ -0,0 +1,260 @@
+
+def palindrome(word):
+    """palindrome(word: str) -> bool
+       Returns a boolean telling whether input string is a palindrome.
+       Is not case sensitive. Does not care about spaces."""
+    word = word.lower().strip()
+    
+    # Base case: if given a single character, it is a palindrome.
+    if len(word) == 1 or len(word) == 0:
+        return True
+
+    # Recursive step: match first and last characters of the string.
+    elif word[0] == word[-1]:
+        newWord = palindrome(word[1:-1])
+        return newWord
+    
+    # If they don't match, it isn't a palindrome.
+    else:
+        return False
+
+
+def digitsToWords(n):
+    """digitsToWords(n: int) -> str
+       Returns a string translation of a positive integer input."""
+    
+    # Dictionary that has digit indices to string definitions
+    digitWordDict = {0:"zero ", 1:"one ", 2:"two ", 3:"three ", 4:"four ",
+                     5:"five ", 6:"six ", 7:"seven ", 8:"eight ", 9:"nine "}
+    
+    # Base case: single digit number
+    if n < 10:
+       return str(digitWordDict[n])
+   
+    # Recursive step: dividing by 10, remainder, and recursing
+    else:
+        newN = n // 10
+        rem = n - 10*newN
+        # print("newN, rem =", str(newN), str(rem))
+        return (digitsToWords(newN) + str(digitWordDict[rem]))
+
+
+def sumToTarget(nums, x):
+    """sumToTarget(nums: list of ints, x: int) -> bool
+       Returns a bool on whether x is the sum of a subset of nums."""
+       
+    # Base case: Compare the single-element list
+    if len(nums) == 1:
+        return nums[0] == x
+    
+    # Pop the first element
+    first = nums.pop(0)
+    
+    # With the rest of the elements, try summing with and without the first element
+    leftBool, rightBool = sumToTarget(nums, x), sumToTarget(nums, x-first)
+    return leftBool or rightBool
+    
+
+def choose(n, k):
+    """choose(n: int, k: int) -> int
+       Returns n choose k."""
+    
+    # Base case: if the result is 1, return 1
+    if k == 0 or k == n:
+        return 1
+    
+    # Recursive step: using the given formula, recurse by adding
+    else:
+        return choose(n-1,k-1) + choose(n-1,k)
+
+
+def main():
+    """main() -> None
+       Executes main game."""
+       
+    print("Welcome to the Recursion Calculator!")
+    
+    # Main while loop - option selection and quitting
+    while True:
+        print("\nChoose a function:\n\
+    1) Palindrome\n\
+    2) digitsToWords\n\
+    3) sumToTarget\n\
+    4) choose\n\
+    9) Quit")
+        
+        # Get initial input
+        opt = input("Option: ")
+        
+        # If not in the valid list, print error message
+        if opt not in ["1","2","3","4","9"]:
+            print("Please enter a valid option!")
+        
+        # Break from while loop; go to end of function main()
+        if opt == "9":
+            break
+        
+        # Palindrome input validation
+        elif opt == "1":
+            
+            # Get initial input
+            pal = input("Enter a word: ")            
+            
+            # Print success message
+            if palindrome(pal):
+                print("\n'"+pal+"' is a palindrome.")
+                
+            # Print failure message
+            else: 
+                print("\n'"+pal+"' is not a palindrome.")
+        
+        # digitsToWords input validation
+        elif opt == "2":
+            
+            # Get initial input, set valid to False
+            dtw = input("Enter a positive integer: ")
+            dtwValid = False
+        
+            # Input validation while loop
+            while not dtwValid:
+                dtwValid = True
+                
+                # Check if each character is a digit
+                for ch in dtw:
+                    if ord(ch) not in range(48,59):
+                        dtwValid = False
+                
+                # Print error message; get next input
+                if dtwValid == False:
+                    print("\nPlease enter a positive integer only.")
+                    dtw = input("Enter a positive integer: ")
+            
+            dtw = int(dtw)
+            
+            # Print execution message
+            print("\n"+str(dtw), "can be read as: ", digitsToWords(dtw))
+        
+        # sumToTarget input validation
+        elif opt == "3":
+            
+            # Get initial input, set valid to False
+            stt = input("Enter a list of numbers and a positive integer (ex:\
+[1,2,3],5): ")
+            sttValid = False
+            
+            # Input validation while loop, initialize some variables
+            while not sttValid:
+                sttValid = True
+                sttList = []
+                current = ""
+                x = ""
+                
+                # Loop backwards by character to find positive x; 
+                # Stops with ','; invalid with '-'
+                for ch in stt[-1:1:-1]:
+                    
+                    # Invalidate negative sign
+                    if ord(ch) == 45:
+                        sttValid = False
+                        break
+                    
+                    # Stop when comma
+                    elif ord(ch) == 44:
+                        x = int(x)
+                        break
+                    
+                    # Append digit
+                    elif ord(ch) in range(48,59):
+                        x = ch + x
+                
+                # Loop fowards and put items into list until ']'
+                for ch in stt:
+                    
+                    # Stop when right bracket
+                    if ord(ch) == 93:
+                        sttList.append(int(current))
+                        break
+                    
+                    # Change number when comma
+                    elif ord(ch) == 44:
+                        sttList.append(int(current))
+                        current = ""
+                    
+                    # Valid digit or negative sign
+                    elif ord(ch) in range(48,59) or ord(ch) == 45:
+                        current += ch
+                    
+                    # No action on space or left bracket
+                    elif ord(ch) not in [32, 91]:
+                        sttValid = False
+                
+                # Print error message and get next input
+                if not sttValid:
+                    print("\nPlease enter the inputs in the correct format, with a positive x.")
+                    stt = input("\nEnter a list of numbers and a positive integer (ex:\
+[1,2,3],5): ")
+                        
+            print(sumToTarget(sttList, x))
+        
+        # choose input validation
+        elif opt == "4":
+            
+            # Get initial input
+            chs = input("Enter total and number to choose (separated by comma): ")
+            chooseValid = False
+            ineq = True
+            
+            # Input validation while loop; assume valid to be True
+            while not chooseValid:
+                chooseValid = True
+                
+                # Initialize some variables
+                chs = chs.strip()
+                n,k = "",""
+                isK = False
+                
+                # Check each character
+                for ch in chs:
+                    
+                    # Check if it is comma or 
+                    if ord(ch) in [44,93]:
+                        isK = True
+                    
+                    # Increment either n or k by 1, based on comma position
+                    elif ord(ch) in range(48, 59):
+                        if isK:
+                            k += ch
+                        else:
+                            n += ch
+                    
+                    # Invalid if any non-numerical character is detected
+                    else:
+                        chooseValid = False
+                
+                # Check if inequality is satisfied
+                if chooseValid:
+                    # 
+                    if int(n) < int(k):
+                        ineq = False
+                        chooseValid = False
+                
+                # Prints and errors
+                if not chooseValid:
+                    
+                    # Print error messages for inequality clause
+                    if not ineq:
+                        print("\nMake sure the first number is larger than the second.")
+                    
+                    # Print error message for nonnegative integers
+                    else:
+                        print("\nPlease enter nonnegative integers separated by a comma only.")
+                    
+                    # Get next input
+                    chs = input("Enter total and number to choose (separated by comma): ")
+                        
+            print(choose(int(n),int(k)))
+
+    # Quit message.
+    print("\nYou have left the Recursion Calculator. Bye!")
+
+main()
---
+++ TimeManagementGame.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 14:58:47 2021
+
+@author: meljel
+"""
+
+# You need to write and use a subclass of your Queue class from Exercise #2 so 
+# dequeue returns the first customer who hasn’t lost patience
+# Customers should have different patience levels (you can use Python’s  random 
+# library)
+# There needs to be an element of “time” to determine whether a customer is 
+# still in line (you can use Python’s time library)
+# The user interface for this game needs to explain how to play, tell the user 
+# when a customer enters the line or leaves because they lost patience, and 
+# when the game is over (the queue is empty)
+
+import random
+import TwoStackQueue as q
+
+def main():
+    """Run the time management game."""
+
+    # Welcome statement.
+    print("Welcome to the time cafe. \nServe all customers before they lose patience!")
+    
+    # Request number of customers until input is valid or program is quit
+    while True:
+        # Gets customer input from user.
+        customers = input("Number of customers in line: ")
+        
+        try:
+            # Check if input is a number.
+            customers = int(customers)
+            
+            # Check if input is positive.
+            if customers > 0:
+                run = True
+                break
+            
+            else:
+                # Print error message telling user to enter a positive integer.
+                print("Please enter a positive integer. (Enter 'Q' to quit)")
+        
+        except:
+            # Check if the user wants to quit the program.
+            if customers[0] == "Q" or customers[0] == 'q':
+                run = False
+                break
+            
+            # Print error message telling user to enter an integer. 
+            print("Please enter an integer.")
+    
+    # Run if user has not quit.
+    if run:
+        print("Okay! Let's play.")
+        
+        # Create an instance of Queue with lunchLine on.
+        line = q.Queue(customers, True)
+        
+        # Enqueue "customer" (represented by their patience)
+        for k in range(customers):
+            # Randomly generate patience given the range [1,customers].
+            line.enqueue(random.randint(1,customers + 1))
+        # patience = [random.randint(1,11) for k in range(customers)]
+        # print("list: " + str(line.getList()))
+        
+        seated, lost = 0,0
+    
+    # Run if user has not quit.
+    while run: # or while run = 1
+        # patience = random.randint(0,10) # PLACEHOLDER
+        
+        # Break the loop if the line has been depleted.
+        if line.length() == 0:
+            break
+        
+        # Initially dequeue the first person in the queue.
+        current = line.dequeue()
+        # If their patience is 0 or None, proceed to the next customer in line.
+        if not current:
+            continue
+        # print("current = " + str(current))
+        
+        # Prompt user with three options for the current customer.
+        print("\nA new customer is waiting. (Patience: " + str(current) + ")")
+        print("\n    (1) Seat customer \n    (2) Wait \n    (3) Quit")
+        
+        i = input("Action (1,2,3): ")
+        
+        # Wait for response from user on selected option until it is valid.
+        while i not in ["1","2","3"]:
+            print("Please choose an action between options 1, 2, and 3.")
+            print("\n    (1) Seat customer \n    (2) Wait \n    (3) Quit")
+            i = input("Action (1,2,3): ")
+        
+        # Seat the customer.
+        if i == "1":
+            print("\nYou sat a new customer.")
+            seated += 1
+        
+        # Make the customer wait; requeue.
+        elif i == "2":
+            line.enqueue(current)
+            print("\nYour customer is now waiting.")
+        
+        # Quit the while loop.
+        elif i == "3":
+            run = False
+            break
+        
+        else:
+            # Failsafe. Shouldn't ever happen.
+            print("[Error] \
+                   Something weird happened (input validation for 3 options)")
+        
+        # Lower each customer's patience by 1. 
+        numLost = line.timePass(1)
+        
+        # Print how many customers are lost.
+        if numLost == 1:
+            print("You lost a customer.")
+        elif numLost > 1:
+            lost += numLost
+            print("Uh oh! You lost", str(numLost), "customers.")
+        
+    # Run if user has quit.    
+    if not run:
+        print("\nYou have quit the game. Goodbye!")
+        raise SystemExit
+    
+    # Print that the queue has been cleared, and the game is over.
+    print("\nThere are no more customers in line. \n\
+You sat", str(seated), "customers and lost ", str(lost), "customers.\n\n\
+Thanks for playing!")
+
+main()
