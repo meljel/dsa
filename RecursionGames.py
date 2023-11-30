403acc175e19e603fcea2f95bf96901c84f1958e
melodyjing <meljel142857@gmail.com>
2023-11-30 16:03:44 -0600

    RecursionGames refactor & cleanup

--- RecursionGames.py
+++ RecursionGames.py
@@ -1,3 +1,5 @@
+# Melody Jing
+# 
 
 def palindrome(word):
     """palindrome(word: str) -> bool
@@ -68,6 +70,164 @@ def choose(n, k):
         return choose(n-1,k-1) + choose(n-1,k)
 
 
+def playPalindrome():
+    # Get initial input
+    pal = input("Enter a word: ")            
+    
+    # Print success message
+    if palindrome(pal):
+        print("\n'"+pal+"' is a palindrome.")
+        
+    # Print failure message
+    else: 
+        print("\n'"+pal+"' is not a palindrome.")
+
+
+def playDigitsToWords():
+    # Get initial input, set valid to False
+    dtw = input("Enter a positive integer: ")
+    valid = False
+
+    # Input validation while loop
+    while not valid:
+        valid = True
+        
+        # Check if each character is a digit
+        for ch in dtw:
+            if ord(ch) not in range(48,59):
+                valid = False
+        
+        # Print error message; get next input
+        if valid == False:
+            print("\nPlease enter a positive integer only.")
+            dtw = input("Enter a positive integer: ")
+    
+    dtw = int(dtw)
+    
+    # Print execution message
+    print("\n"+str(dtw), "can be read as:", digitsToWords(dtw))
+
+
+def playSumToTarget():
+    # Get initial input, set valid to False
+    stt = input("Enter a list of numbers and a positive integer (ex: [1,2,3],5): ")
+    valid = False
+    
+    # Input validation while loop, initialize some variables
+    while not valid:
+        valid = True
+        sttList = []
+        current = ""
+        x = ""
+        
+        # Loop backwards by character to find positive x; 
+        # Stops with ','; invalid with '-'
+        for ch in stt[-1:1:-1]:
+            
+            # Invalidate negative sign
+            if ord(ch) == 45:
+                valid = False
+                break
+            
+            # Stop when comma
+            elif ord(ch) == 44:
+                x = int(x)
+                break
+            
+            # Append digit
+            elif ord(ch) in range(48,59):
+                x = ch + x
+        
+        # Loop fowards and put items into list until ']'
+        for ch in stt:
+            
+            # Stop when right bracket
+            if ord(ch) == 93:
+                sttList.append(int(current))
+                break
+            
+            # Change number when comma
+            elif ord(ch) == 44:
+                sttList.append(int(current))
+                current = ""
+            
+            # Valid digit or negative sign
+            elif ord(ch) in range(48,59) or ord(ch) == 45:
+                current += ch
+            
+            # No action on space or left bracket
+            elif ord(ch) not in [32, 91]:
+                valid = False
+        
+        # Print error message and get next input
+        if not valid:
+            print("\nPlease enter the inputs in the correct format, with a\
+positive x.")
+            stt = input("\nEnter a list of numbers and a positive integer (ex:\
+[1,2,3],5): ")
+    
+    s = " NOT"
+    if sumToTarget(sttList, x):
+        s = ""
+    print("The sum", x, "can"+s, "be formed from elements of", str(sttList)+".")
+
+
+def playChoose():
+    # Get initial input
+    chs = input("Enter total and number to choose (separated by comma): ")
+    valid = False
+    ineq = True
+    
+    # Input validation while loop; assume valid to be True
+    while not valid:
+        valid = True
+        
+        # Initialize some variables
+        chs = chs.strip()
+        n,k = "",""
+        isK = False
+        
+        # Check each character
+        for ch in chs:
+            
+            # Check if it is comma or 
+            if ord(ch) in [44,93]:
+                isK = True
+            
+            # Increment either n or k by 1, based on comma position
+            elif ord(ch) in range(48, 59):
+                if isK:
+                    k += ch
+                else:
+                    n += ch
+            
+            # Invalid if any non-numerical character is detected
+            else:
+                valid = False
+        
+        # Check if inequality is satisfied
+        if valid:
+            if int(n) < int(k):
+                ineq = False
+                valid = False
+        
+        # Prints and errors
+        if not valid:
+            
+            # Print error messages for inequality clause
+            if not ineq:
+                print("\nMake sure the first number is larger than the second.")
+            
+            # Print error message for nonnegative integers
+            else:
+                print("\nPlease enter nonnegative integers separated by a comma only.")
+            
+            # Get next input
+            chs = input("Enter total and number to choose (separated by comma): ")
+    
+    print(n, "choose", k, "is", str(choose(int(n),int(k)))+".")
+
+
 def main():
     """main() -> None
        Executes main game."""
@@ -91,168 +251,19 @@ def main():
             print("Please enter a valid option!")
         
         # Break from while loop; go to end of function main()
-        if opt == "9":
-            break
+        if opt == "9": break
         
         # Palindrome input validation
-        elif opt == "1":
-            
-            # Get initial input
-            pal = input("Enter a word: ")            
-            
-            # Print success message
-            if palindrome(pal):
-                print("\n'"+pal+"' is a palindrome.")
-                
-            # Print failure message
-            else: 
-                print("\n'"+pal+"' is not a palindrome.")
+        elif opt == "1": playPalindrome()
         
         # digitsToWords input validation
-        elif opt == "2":
-            
-            # Get initial input, set valid to False
-            dtw = input("Enter a positive integer: ")
-            dtwValid = False
-        
-            # Input validation while loop
-            while not dtwValid:
-                dtwValid = True
-                
-                # Check if each character is a digit
-                for ch in dtw:
-                    if ord(ch) not in range(48,59):
-                        dtwValid = False
-                
-                # Print error message; get next input
-                if dtwValid == False:
-                    print("\nPlease enter a positive integer only.")
-                    dtw = input("Enter a positive integer: ")
-            
-            dtw = int(dtw)
-            
-            # Print execution message
-            print("\n"+str(dtw), "can be read as: ", digitsToWords(dtw))
+        elif opt == "2": playDigitsToWords()
         
         # sumToTarget input validation
-        elif opt == "3":
-            
-            # Get initial input, set valid to False
-            stt = input("Enter a list of numbers and a positive integer (ex:\
-[1,2,3],5): ")
-            sttValid = False
-            
-            # Input validation while loop, initialize some variables
-            while not sttValid:
-                sttValid = True
-                sttList = []
-                current = ""
-                x = ""
-                
-                # Loop backwards by character to find positive x; 
-                # Stops with ','; invalid with '-'
-                for ch in stt[-1:1:-1]:
-                    
-                    # Invalidate negative sign
-                    if ord(ch) == 45:
-                        sttValid = False
-                        break
-                    
-                    # Stop when comma
-                    elif ord(ch) == 44:
-                        x = int(x)
-                        break
-                    
-                    # Append digit
-                    elif ord(ch) in range(48,59):
-                        x = ch + x
-                
-                # Loop fowards and put items into list until ']'
-                for ch in stt:
-                    
-                    # Stop when right bracket
-                    if ord(ch) == 93:
-                        sttList.append(int(current))
-                        break
-                    
-                    # Change number when comma
-                    elif ord(ch) == 44:
-                        sttList.append(int(current))
-                        current = ""
-                    
-                    # Valid digit or negative sign
-                    elif ord(ch) in range(48,59) or ord(ch) == 45:
-                        current += ch
-                    
-                    # No action on space or left bracket
-                    elif ord(ch) not in [32, 91]:
-                        sttValid = False
-                
-                # Print error message and get next input
-                if not sttValid:
-                    print("\nPlease enter the inputs in the correct format, with a positive x.")
-                    stt = input("\nEnter a list of numbers and a positive integer (ex:\
-[1,2,3],5): ")
-                        
-            print(sumToTarget(sttList, x))
+        elif opt == "3": playSumToTarget()
         
         # choose input validation
-        elif opt == "4":
-            
-            # Get initial input
-            chs = input("Enter total and number to choose (separated by comma): ")
-            chooseValid = False
-            ineq = True
-            
-            # Input validation while loop; assume valid to be True
-            while not chooseValid:
-                chooseValid = True
-                
-                # Initialize some variables
-                chs = chs.strip()
-                n,k = "",""
-                isK = False
-                
-                # Check each character
-                for ch in chs:
-                    
-                    # Check if it is comma or 
-                    if ord(ch) in [44,93]:
-                        isK = True
-                    
-                    # Increment either n or k by 1, based on comma position
-                    elif ord(ch) in range(48, 59):
-                        if isK:
-                            k += ch
-                        else:
-                            n += ch
-                    
-                    # Invalid if any non-numerical character is detected
-                    else:
-                        chooseValid = False
-                
-                # Check if inequality is satisfied
-                if chooseValid:
-                    # 
-                    if int(n) < int(k):
-                        ineq = False
-                        chooseValid = False
-                
-                # Prints and errors
-                if not chooseValid:
-                    
-                    # Print error messages for inequality clause
-                    if not ineq:
-                        print("\nMake sure the first number is larger than the second.")
-                    
-                    # Print error message for nonnegative integers
-                    else:
-                        print("\nPlease enter nonnegative integers separated by a comma only.")
-                    
-                    # Get next input
-                    chs = input("Enter total and number to choose (separated by comma): ")
-                        
-            print(choose(int(n),int(k)))
+        elif opt == "4": playChoose()
 
     # Quit message.
     print("\nYou have left the Recursion Calculator. Bye!")
